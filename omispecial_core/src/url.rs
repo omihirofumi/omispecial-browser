e0e98476bd0b16b6e83c4af1582f793592983a6d
Hirofumi Omi <hirofumi.omi@3-shake.com>
2024-11-16 21:59:44 +0900

    first commit

---
+++ omispecial_core/src/url.rs
@@ -0,0 +1,55 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Url {
+    url: String,
+    host: String,
+    port: String,
+    path: String,
+    searchpart: String,
+}
+
+impl Url {
+    pub fn new(url: String) -> Self {
+        Self {
+            url,
+            host: "".to_string(),
+            port: "".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        }
+    }
+    pub fn parse(&mut self) -> Result<Self, String> {
+        if !self.is_http() {
+            return Err("Only HTTP scheme is supported.".to_string());
+        }
+    }
+    fn is_http(&mut self) -> bool {
+        if self.url.contains("http://") {
+            return true
+        }
+        return false
+    }
+    fn extract_host(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][..index].to_string()
+        } else {
+            url_parts[0].to_string()
+        }
+    }
+    fn extract_port(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][index + 1..].to_string()
+        } else {
+            "80".to_string()
+        }
+    }
+}
